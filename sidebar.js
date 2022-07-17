e0993617d48a88686f0ffd5dcbf2dc3e846421c4
Tim Greller <timlg07@gmail.com>
2022-07-17 21:20:25 +0200

    minor improvements (naming & types)

--- sidebar.js
+++ sidebar.js
@@ -6,24 +6,23 @@
  * your sidebar should have from its original position - apply it to the `top` property and use relative positioning.
  * That's all you need. If you have a sticky/fixed header on your page (which is not in the document flow due to its
  * position) you can specify it as third argument and its height will get accounted for in the calculation process.
- * 
+ *
  * @param {string} sidebarContainerSelector The CSS/query-selector for your sidebar container element
  * @param {string} sidebarSelector The CSS/query-selector for your sidebar element
- * @param {string} stickyHeaderSelector The CSS/query-selector for your sticky/fixed header
+ * @param {string | null} stickyHeaderSelector The CSS/query-selector for your sticky/fixed header
  * @param {Number} marginTop Space between the top of the browser (or bottom of the sticky header if given) and the sidebar.
  * @param {Number} marginBottom Space between the bottom of the browser viewport and the sidebar.
- * 
+ *
  * @returns {Function} A function that can be called to enforce the boundaries and therefore prohibit overflow. It should
  *                     be called when the height of the sidebar changes without scrolling being involved.
  */
 function betterStickySidebar({
-        sidebarContainerSelector, 
-        sidebarSelector, 
-        stickyHeaderSelector = null, 
-        marginTop = 0, 
-        marginBottom = 0
-    }) 
-{
+                                 sidebarContainerSelector,
+                                 sidebarSelector,
+                                 stickyHeaderSelector = null,
+                                 marginTop = 0,
+                                 marginBottom = 0
+                             }) {
     let prevScrollY = window.scrollY;
     let currentSidebarTop = 0;
 
@@ -59,22 +58,24 @@ function betterStickySidebar({
      * recalculates the layout, which is exactly when we want to recalculate the sidebar position as well.
      * (more information: https://stackoverflow.com/a/44779316/6336728)
      */
-    window.addEventListener('scroll', e => {
+    window.addEventListener('scroll', onScroll);
+
+    function onScroll() {
         const alwaysStickToTop = isSmallerThanViewport();
         if (window.scrollY < prevScrollY || alwaysStickToTop) {
-            limitByViewportTop(alwaysStickToTop); 
+            limitByViewportTop(alwaysStickToTop);
         } else {
             limitByViewportBottom();
         }
         prevScrollY = window.scrollY;
-    });
+    }
 
     function limitByViewportTop(force = false) {
         const delta = sidebar.getBoundingClientRect().top - getStickyHeaderHeight() - marginTop;
-        const abovesidebar = delta > 0;
+        const aboveSidebar = delta > 0;
 
-        // We are scrolling above the element => reduce the top space to keep it sticked to the top
-        if (abovesidebar || force) {
+        // We are scrolling above the element => reduce the top space to keep it stuck to the top
+        if (aboveSidebar || force) {
             updateSidebarPosition(currentSidebarTop - delta);
         } else {
             // check bounds
@@ -86,11 +87,11 @@ function betterStickySidebar({
         const browserBottom = window.innerHeight;
         const sidebarBottom = sidebar.getBoundingClientRect().bottom;
         const delta = browserBottom - sidebarBottom - marginBottom;
-        const belowsidebar = delta > 0;
+        const belowSidebar = delta > 0;
 
         // We are scrolling below the element & it would scroll out of the viewport. 
-        // => increase top space to keep it sticked to the bottom
-        if (belowsidebar) {
+        // => increase top space to keep it stuck to the bottom
+        if (belowSidebar) {
             updateSidebarPosition(currentSidebarTop + delta);
         } else {
             // check bounds
